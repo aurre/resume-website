f54f4785b27608ebe194cf3696745502e3b05604
Raysa Aurrecochea <afrodita@Raysas-MBP.lan>
2021-10-07 16:38:54 -0400

    add server for contact form end point

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # React JS Resume Web App
 
-### <a href="">LIVE DEMO</a>
+### <a href="https://raysa.me">LIVE DEMO</a>
 
 ## Description
 
 This is a ReactJS based personal resume website to reflect my experience as Software Developer.
+
+## How to run locally. Run from root foler the following 2 commands:
+
+# npm run start
+
+# node server/index.js
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,93 +1,103 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Contact extends Component {
-   render() {
+const Contact = ({ data }) => {
+   if (data) {
+      var name = data.name;
+      var phone = data.phone;
+      var email = data.email;
+      var message = data.contactmessage;
+   };
+   const [status, setStatus] = useState("Submit");
+   const [error, setError] = useState(false);
+   const [success, setSuccess] = useState(false);
 
-      if (this.props.data) {
-         var name = this.props.data.name;
-         var phone = this.props.data.phone;
-         var email = this.props.data.email;
-         var message = this.props.data.contactmessage;
-         var warningMsg = this.props.data.warningMsg;
-      }
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      setStatus("Sending...");
+      const { name, email, message } = e.target.elements;
+      let details = {
+         name: name.value,
+         email: email.value,
+         message: message.value,
+      };
+      let response = await fetch("http://localhost:5000/contact", {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json;charset=utf-8",
+         },
+         body: JSON.stringify(details),
+      });
+      setStatus("Submit");
+      let result = await response.json();
+      (result.status === 'ERROR') ? setError(true) : setSuccess(true);
 
-      return (
-         <section id="contact">
+   };
+   return (
+      <section id="contact">
 
-            <div className="row section-head">
+         <div className="row section-head">
 
-               <div className="two columns header-col">
+            <div className="two columns header-col">
 
-                  <h1><span>Get In Touch.</span></h1>
-
-               </div>
-
-               <div className="ten columns">
-
-                  <p className="lead">{message}</p>
-                  <p className="lead warning">{warningMsg}</p>
-
-               </div>
+               <h1><span>Get In Touch.</span></h1>
 
             </div>
 
-            <div className="row">
-               <div className="eight columns">
+            <div className="ten columns">
 
-                  <form action="" method="post" id="contactForm" name="contactForm">
-                     <fieldset>
-
-                        <div>
-                           <label htmlFor="contactName">Name <span className="required">*</span></label>
-                           <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange} />
-                        </div>
-
-                        <div>
-                           <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                           <input type="text" defaultValue="" size="35" id="contactEmail" name="contactEmail" onChange={this.handleChange} />
-                        </div>
-
-                        <div>
-                           <label htmlFor="contactSubject">Subject</label>
-                           <input type="text" defaultValue="" size="35" id="contactSubject" name="contactSubject" onChange={this.handleChange} />
-                        </div>
-
-                        <div>
-                           <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                           <textarea cols="50" rows="15" id="contactMessage" name="contactMessage"></textarea>
-                        </div>
-
-                        <div>
-                           <button className="submit">Submit</button>
-                           <span id="image-loader">
-                              <img alt="" src="images/loader.gif" />
-                           </span>
-                        </div>
-                     </fieldset>
-                  </form>
-
-                  <div id="message-warning"> Error boy</div>
-                  <div id="message-success">
-                     <i className="fa fa-check"></i>Your message was sent, thank you!<br />
-                  </div>
-               </div>
-
-
-               <aside className="four columns footer-widgets">
-                  <div className="widget widget_contact">
-
-                     <h4>Email and Phone</h4>
-                     <p className="address">
-                        {name}<br />
-                        {email} <br />
-                        <span>{phone}</span>
-                     </p>
-                  </div>
-               </aside>
+               <p className="lead">{message}</p>
             </div>
-         </section>
-      );
-   }
+
+         </div>
+
+         <div className="row">
+            <div className="eight columns">
+
+               <form id="contactForm" name="contactForm" onSubmit={handleSubmit}>
+                  <fieldset>
+                     <div>
+                        <label htmlFor="name">Name <span className="required">*</span></label>
+                        <input type="text" id="name" defaultValue="" size="35" name="contactName" required />
+                     </div>
+
+                     <div>
+                        <label htmlFor="email">Email <span className="required">*</span></label>
+                        <input type="email" id="email" defaultValue="" size="35" name="contactEmail" required />
+                     </div>
+
+                     <div>
+                        <label htmlFor="message">Message<span className="required">*</span></label>
+                        <textarea cols="50" rows="15" id="message" name="contactMessage"></textarea>
+                     </div>
+                     <div>
+                        {error && <div id="message-warning">Something went wrong while sending your message.</div>}
+                        {success && <div id="message-success">
+                           <i className="fa fa-check"></i>Your message was sent, thank you!<br />
+                        </div>}
+                        <button type="submit" className="submit">{status}</button>
+                        <span id="image-loader">
+                           <img alt="" src="images/loader.gif" />
+                        </span>
+                     </div>
+                  </fieldset>
+               </form>
+            </div>
+
+
+            <aside className="four columns footer-widgets">
+               <div className="widget widget_contact">
+
+                  <h4>Email and Phone</h4>
+                  <p className="address">
+                     {name}<br />
+                     {email} <br />
+                     <span>{phone}</span>
+                  </p>
+               </div>
+            </aside>
+         </div>
+      </section>
+   );
 }
 
 export default Contact;
